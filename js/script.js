9c2004696b7336296113ec2ec242cd9b6ab86557
Omar-Codess <omartaki8@gmail.com>
2022-10-14 19:33:11 +0200

    aggiunta funzione checkPalindrome

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-alert("ciao")
+// Chiedo una parola all'utente
+const userWord = prompt("Inserisci una parola")
+console.log(userWord);
+
+
+// Controllo se è palindroma creando una funzione
+
+function checkPalindrome(userWord) {
+    const length = userWord.length;
+    let result = "";
+    for (let i = 0; i < length / 2; i++) {
+
+        if(userWord[i] !== userWord[length - 1 - i]){
+            result = `La parola inserita non è palindroma`;
+        } else {
+            result = `La parola inserita è palindroma`
+        }
+    }
+    return result
+} 
+// Restituisco il risultato
+const value = checkPalindrome(userWord);
+console.log(value);
